c00b3c167b98265f45429dfac10583d52c8dfd20
pratik-hanmante <84140896+pratik-hanmante@users.noreply.github.com>
2025-04-16 23:17:40 +0530

    Update User.java

--- userservice/src/main/java/com/fitness/userservice/model/User.java
+++ userservice/src/main/java/com/fitness/userservice/model/User.java
@@ -8,7 +8,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 
 /**
- * Maps to the "users" table in the database.
+ * Maps to the "users" table in db
  */
 @Entity
 @Table(name = "users")
